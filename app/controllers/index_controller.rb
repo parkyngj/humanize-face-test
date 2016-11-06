21d2238e0bdc83d365591a5a6967ccac6c7c4e91
spark <spark@MacBook-Air-2.local>
2016-11-05 21:17:51 -0700

    The chart is AJAXed Bruh

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @thing = HumanizeService.get_the_thing("tests/5")
+    @things = HumanizeService.get_the_things("tests")
+    @thing = HumanizeService.get_the_thing("tests", "5")
   end
 end
