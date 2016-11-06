2b0ec882149c018d03d2d7fa1f4172f55b916f4b
spark <spark@MacBook-Air-2.local>
2016-11-05 17:19:05 -0700

    Modify chart metrics

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -227,13 +227,13 @@ $(function () {
         yAxis: {
             min: 0,
             title: {
-                text: 'Rainfall (mm)'
+                text: 'Gucci (mm)'
             }
         },
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
